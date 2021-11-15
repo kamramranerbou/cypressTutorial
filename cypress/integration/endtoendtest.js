607f869aa843cf8f9ce07fba51a72fd26f93276e
riponmd <mdabdullahalkamran.ripon@dataport.de>
2021-11-15 11:11:42 +0100

    Update

--- cypress/integration/endtoendtest.js
+++ cypress/integration/endtoendtest.js
@@ -3,36 +3,34 @@
 const { capitalize } = require("lodash")
 
 describe ('Test work', () => {
-        
-    beforeEach(() => {
-        cy.login()
-
-    })
-    
+            
     it('Testfaelle - 1', () => {
         cy.login('Tauto_anbieterverwalter', '#Gadefk12')
         cy.visit("https://qs.lugas.dsecure-bdc.dataport.de/frontend/Anbieterverwaltung")
+        cy.wait(2500)
         
     })
 
     it('Testfaelle - 2', () => {
         cy.login('Tauto_anbieterverwalter', '#Gadefk12')
         cy.visit("https://qs.lugas.dsecure-bdc.dataport.de/frontend/Anbieterverwaltung")
+        cy.wait(2500)
         
     })
 
     it('Testfaelle - 3', () => {
         cy.login('Tauto_auswerter', '#Gaedfk21')
         cy.visit("https://qs.lugas.dsecure-bdc.dataport.de/frontend/Anbieterverwaltung")
+        cy.wait(2500)
         
     })
-    it('Testfaelle - 4', () => {
-        cy.login('Tauto_auswerter', '#Gaedfk21')
+
+    it.only('Testfaelle - 4', () => {
+        cy.login('test@behorde.dglueck.de', '#Gandalf1')
         cy.visit("https://qs.lugas.dsecure-bdc.dataport.de/frontend/Anbieterverwaltung")
+
+        cy.get('[id="mat-input-0"]').click().type('kamran')
         
     })
-
-    
-
 })
 
